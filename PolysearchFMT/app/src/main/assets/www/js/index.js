a5cbce02ef134b0f5be9fc4f66c033ccc1523943
Iheb Chagra <ihebchagra@gmail.com>
2022-08-24 18:15:07 +0100

    fix cookie bug

--- PolysearchFMT/app/src/main/assets/www/js/index.js
+++ PolysearchFMT/app/src/main/assets/www/js/index.js
@@ -33,6 +33,7 @@ function setCookie(cname, cvalue, exdays) {
 	let expires = "expires="+ d.toUTCString();
 	if (Array.isArray(cvalue)){
 		cvalue = JSON.stringify(cvalue);
+		cvalue = cvalue.replaceAll("%","%25")
 	}
 	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 } 
